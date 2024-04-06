ae80e1bda0ee36b5316909f935f4674b2b43f164
Maverick Wolfe <maverick.m.wolfe@gmail.com>
2024-04-05 21:31:34 -0700

    added CSS loaders and babel to webpack

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -1,6 +1,3 @@
-const path = require('path');
+const path = require("path");
 
-module.exports = (app) =>
-  app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'))
-  );
+module.exports = (app) => app.get("/", (req, res) => res.sendFile(path.join(__dirname, "../client/dist/index.html")));
